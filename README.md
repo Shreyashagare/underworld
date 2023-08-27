2227e22a2228b96b66bf65cc881243c4a11e11ee
shreayshagare <agareshreyash26@gmail.com>
2023-08-27 16:43:20 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # underworld
+
+<!-- pleasae view in 1920 by 1080 px -->
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let background = document.getElementById("background");
-let lava = document.getElementById("lava");
+// acessing all the the required element
+
 let leftbottom = document.getElementById("left-bottom");
 let lefttop = document.getElementById("left-top");
 let righttop = document.getElementById("right-top");
@@ -10,22 +10,22 @@ let text = document.getElementById("text");
 let frontrock = document.getElementById("front-rock");
 let bottombg = document.getElementById("background-bg");
 
+//add a event of scrolling vericaly
+
 window.addEventListener("scroll", () => {
   let value = window.scrollY;
 
-  console.log(value);
-  if (value < 400) {
+  console.log(value); // just to get the scroll value
+  //creating if else loop so that after the scroll value reaches 420 the the animating stops
+  if (value < 420) {
     bottomrocks.style.display = "block";
-    bottomrocks.style.top = value * 2.5 + "px";
+    bottomrocks.style.top = value * 2.5 + "px"; //animating the layers
     text.style.display = "block";
     text.style.marginTop = value * 2.5 + "px";
     frontrock.style.left = value * -2.5 + "px";
     toprocks.style.top = value * -2.5 + "px";
-    // bottomright.style.right = value * -10.5 + "px";
   } else {
     text.style.display = "none";
     bottomrocks.style.display = "none";
   }
-  bottombg.style.top = value * -2.5 + "px";
 });
-ScrollReveal().reveal(".sec");
